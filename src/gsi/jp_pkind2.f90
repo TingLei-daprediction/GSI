125dc2a1be08d835a06b497d3057a4ead11c5f44
TingLei-daprediction <leiting2002@gmail.com>
2023-12-13 20:21:18 +0000

    merge with current EMC GSI

---
+++ src/gsi/jp_pkind2.f90
@@ -0,0 +1,6 @@
+!===================================================================
+module jp_pkind2 ! Integer kinds for helf- and fourth-precision integers
+!===================================================================
+integer,parameter:: hpi=selected_int_kind(3),&
+                    fpi=selected_int_kind(2)
+end module jp_pkind2
